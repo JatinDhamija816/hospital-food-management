a6ba42374f4d8e224faebb5cecbd4ea1faa7c3e7
jdhamija816 <jdhamija816@gmail.com>
2025-01-12 11:49:24 +0530

    bug fix

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -5,7 +5,7 @@ const LandingPage = () => {
       <p className="text-sm text-gray-600 text-center">
         Note: After adding, updating, or deleting data, if the changes are not
         visible, please refresh the page or click on the logo button (HFM) on
-        the left side.
+        the left side or move to route /*.
       </p>
     </div>
   );
